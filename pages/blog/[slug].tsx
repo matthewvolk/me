a277a34065808a63486c9f6816f7f520077b0861
Matthew Volk <volkmattj@gmail.com>
2021-09-23 13:32:32 -0500

    implement next mdx remote

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,24 +3,23 @@ import { promises as fs } from "fs";
 import Nav from "../../components/nav";
 import styles from "../../styles/blog.module.scss";
 import Footer from "../../components/footer";
+import { GetStaticPropsContext } from "next";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import matter from "gray-matter";
 
-const Post = () => {
+const components = {
+  h1: (props: any) => <h1 className={styles.postTitle}>{props.children}</h1>,
+  p: (props: any) => <p className={styles.postContent}>{props.children}</p>,
+};
+
+const Post = ({ source, frontMatter }: any) => {
   return (
     <div className={styles.font}>
       <Nav />
       <div className={styles.container}>
-        <h1 className={styles.postTitle}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit!
-        </h1>
-        <p className={styles.postMeta}>by Matthew Volk on 11.12.22</p>
-        <p className={styles.postMeta}>6 min read</p>
         <div className={styles.postContentContainer}>
-          <p className={styles.postContent}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
-            quo voluptate fuga tenetur fugit blanditiis accusantium praesentium,
-            voluptas magnam laboriosam officia nulla. Magnam labore beatae
-            inventore nulla minus porro? Odio?
-          </p>
+          <MDXRemote {...source} components={components} />
         </div>
         <Footer />
       </div>
@@ -29,10 +28,10 @@ const Post = () => {
 };
 
 export async function getStaticPaths() {
-  const blogDirectory = path.join(process.cwd(), "_posts");
-  const fileNames = await fs.readdir(blogDirectory);
+  const blogPostsDirectoryPath = path.join(process.cwd(), "_posts");
+  const blogPostFileNames = await fs.readdir(blogPostsDirectoryPath);
 
-  const allSlugs = fileNames.map((filename) => {
+  const allSlugs = blogPostFileNames.map((filename) => {
     const slug = `${filename.substring(0, filename.length - 4)}`;
     return slug;
   });
@@ -51,8 +50,30 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps() {
-  return { props: {} };
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+  let fileToFind: string;
+  if (params) {
+    if (typeof params.slug === "string") {
+      fileToFind = params.slug + ".mdx";
+    } else if (Array.isArray(params.slug)) {
+      fileToFind = params.slug[0] + ".mdx";
+    } else {
+      fileToFind = "";
+    }
+  } else {
+    fileToFind = "";
+  }
+
+  const blogPostsDirectoryPath = path.join(process.cwd(), "_posts");
+  const blogPostFileNames = await fs.readdir(blogPostsDirectoryPath);
+
+  const postFileName = blogPostFileNames.find((file) => file === fileToFind);
+  const pathToPostFileName = path.join(blogPostsDirectoryPath, postFileName!);
+  const postFileContent = await fs.readFile(pathToPostFileName, "utf8");
+  const { content, data } = matter(postFileContent);
+  const mdxSource = await serialize(content, { scope: data });
+
+  return { props: { source: mdxSource, frontMatter: data } };
 }
 
 export default Post;
