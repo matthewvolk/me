f9ab15238621f6a4573a083137179033127dc27b
Matthew Volk <volkmattj@gmail.com>
2021-10-02 17:04:36 -0500

    change favicon

--- components/seo.tsx
+++ components/seo.tsx
@@ -54,7 +54,27 @@ const SEO = ({ title, description, image, url }: SEOProps) => {
             : "My name is Matt and I'm a software developer living in Austin, TX."
         }
       />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#000034" />
+      <meta name="theme-color" content="#000034"></meta>
     </Head>
   );
 };
