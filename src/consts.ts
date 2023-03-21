51db5305531fff7a10af2247296da393f9310fe7
Matthew Volk <volkmattj@gmail.com>
2023-03-21 15:39:33 -0500

    feat(common): migrate to astro

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Matthew Volk - Software Engineer";
+export const SITE_DESCRIPTION =
+  "A personal website for Matthew Volk, a software engineer.";
