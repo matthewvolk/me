63659c01f7841494daa88edff6eec82228047fcc
Matthew Volk <volkmattj@gmail.com>
2023-06-21 19:55:26 -0500

    fix(style): add prettier-plugin-tailwindcss

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,9 @@
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-astro")],
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  pluginSearchDirs: false,
   overrides: [
     {
       files: "*.astro",
