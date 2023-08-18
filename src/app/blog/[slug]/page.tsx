3993e5134aa02f96df6d6a2d121c9ebc8c1cc50d
Matthew Volk <volkmattj@gmail.com>
2023-08-17 23:21:50 -0500

    feat(content): refactor blog post generation

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,50 +1,10 @@
-import { readdirSync, readFileSync } from "node:fs";
-import path from "node:path";
-
-import matter from "gray-matter";
 import { notFound } from "next/navigation";
-import { remark } from "remark";
-import remarkHtml from "remark-html";
 
 import { NavBar } from "@/components/navbar";
-
-const contentDir = path.join(process.cwd(), "src/content");
-
-export interface PostMeta {
-  slug: string;
-  title: string;
-  description: string;
-  published: string;
-  updated?: string;
-}
-
-const getPostsMeta = (): PostMeta[] =>
-  readdirSync(contentDir)
-    .map((fileName) => {
-      const meta = matter(
-        readFileSync(path.join(contentDir, fileName), "utf-8"),
-      );
-
-      return {
-        slug: fileName.replace(/\.md$/, ""),
-        title: meta.data.title,
-        description: meta.data.description,
-        published: meta.data.published,
-        updated: meta.data.updated,
-      };
-    })
-    .sort(
-      (a, b) =>
-        (b.updated
-          ? new Date(b.updated).valueOf()
-          : new Date(b.published).valueOf()) -
-        (a.updated
-          ? new Date(a.updated).valueOf()
-          : new Date(a.published).valueOf()),
-    );
+import { blog, blogs } from "@/content/generate";
 
 export function generateStaticParams() {
-  const posts = getPostsMeta();
+  const posts = blogs();
 
   return posts.map((post) => ({ slug: post.slug }));
 }
@@ -56,51 +16,33 @@ interface PostProps {
 }
 
 export function generateMetadata({ params }: PostProps) {
-  const posts = getPostsMeta();
+  const posts = blogs();
   const post = posts.find((post) => post.slug === params.slug);
 
   return {
-    title: post ? `${post.title} - Matthew Volk` : "Not Found",
+    title: post ? `${post.data.title} - Matthew Volk` : "Not Found",
   };
 }
 
-const getPost = async (slug: string): Promise<PostMeta & { html: string }> => {
-  const meta = matter(
-    readFileSync(path.join(contentDir, `${slug}.md`), "utf-8"),
-  );
-
-  const content = await remark().use(remarkHtml).process(meta.content);
-  const html = content.toString();
-
-  return {
-    slug,
-    title: meta.data.title,
-    description: meta.data.description,
-    published: meta.data.published,
-    updated: meta.data.updated,
-    html,
-  };
-};
-
 const BlogPost = async ({ params }: PostProps) => {
-  const posts = getPostsMeta();
+  const posts = blogs();
   const found = posts.find((post) => post.slug === params.slug);
 
   if (!found) {
     return notFound();
   }
 
-  const post = await getPost(params.slug);
+  const post = await blog(params.slug);
 
   return (
     <main className="mx-auto flex min-h-screen max-w-7xl flex-col p-4">
       <NavBar />
       <div className="w-full max-w-3xl self-center">
         <div className="py-4">
-          <h1 className="py-4 text-5xl font-bold">{post.title}</h1>
+          <h1 className="py-4 text-5xl font-bold">{post.data.title}</h1>
           <div className="pt-2 text-sm italic text-neutral-600 dark:text-neutral-200">
-            {post.updated && <p>Updated: {post.updated}</p>}
-            <p>Published: {post.published}</p>
+            {post.data.updated && <p>Updated: {post.data.updated}</p>}
+            <p>Published: {post.data.published}</p>
           </div>
         </div>
         <div className="py-4"></div>
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,54 +1,16 @@
-import { readdirSync, readFileSync } from "node:fs";
-import path from "node:path";
-
-import matter from "gray-matter";
 import { Metadata } from "next";
 import Link from "next/link";
 
 import { NavBar } from "@/components/navbar";
+import { blogs } from "@/content/generate";
 
 export const metadata: Metadata = {
   title: "Blog - volk.dev",
   description: "Software Engineer",
 };
 
-const contentDir = path.join(process.cwd(), "src/content");
-
-export interface PostMeta {
-  slug: string;
-  title: string;
-  description: string;
-  published: string;
-  updated?: string;
-}
-
-const getPostsMeta = (): PostMeta[] =>
-  readdirSync(contentDir)
-    .map((fileName) => {
-      const meta = matter(
-        readFileSync(path.join(contentDir, fileName), "utf-8"),
-      );
-
-      return {
-        slug: fileName.replace(/\.md$/, ""),
-        title: meta.data.title,
-        description: meta.data.description,
-        published: meta.data.published,
-        updated: meta.data.updated,
-      };
-    })
-    .sort(
-      (a, b) =>
-        (b.updated
-          ? new Date(b.updated).valueOf()
-          : new Date(b.published).valueOf()) -
-        (a.updated
-          ? new Date(a.updated).valueOf()
-          : new Date(a.published).valueOf()),
-    );
-
 const Blog = () => {
-  const posts = getPostsMeta();
+  const posts = blogs();
   return (
     <main className="mx-auto flex min-h-screen max-w-7xl flex-col p-4">
       <NavBar />
@@ -56,15 +18,15 @@ const Blog = () => {
       <h1>Posts</h1>
       {posts.map((post) => (
         <Link href={`/blog/${post.slug}`} key={post.slug}>
-          <h2>{post.title}</h2>
-          <p>{post.description}</p>
-          {post.updated ? (
+          <h2>{post.data.title}</h2>
+          <p>{post.data.description}</p>
+          {post.data.updated ? (
             <p>
-              <span>Updated:</span> {post.updated}
+              <span>Updated:</span> {post.data.updated}
             </p>
           ) : (
             <p>
-              <span>Published:</span> {post.published}
+              <span>Published:</span> {post.data.published}
             </p>
           )}
         </Link>
