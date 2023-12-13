dc9fef02b9a31c4f390f7016dbf1a0d454833730
Matthew Volk <volkmattj@gmail.com>
2023-12-13 02:10:05 -0600

    feat(http): better suspense boundaries (#31)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,22 +1,14 @@
-import Image from "next/image";
 import Link from "next/link";
 import * as React from "react";
+import { Suspense } from "react";
 
+import { GitHubAvatar } from "@/components/github-avatar";
 import { MainNav } from "@/components/main-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Skeleton } from "@/components/ui/skeleton";
 import { siteConfig } from "@/config/site";
-import { env } from "@/lib/env.mjs";
 
-const MainLayout = async ({ children }: React.PropsWithChildren) => {
-  const githubAvatar = async () => {
-    const response = await fetch("https://api.github.com/users/matthewvolk", {
-      headers: { authorization: `bearer ${env.GITHUB_PAT}` },
-    });
-    const data = await response.json();
-
-    return data.avatar_url as string;
-  };
-
+const MainLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <div className="flex min-h-screen flex-col">
       <header className="container z-40">
@@ -27,13 +19,13 @@ const MainLayout = async ({ children }: React.PropsWithChildren) => {
         <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
           <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
             <Link href="/">
-              <Image
-                src={await githubAvatar()}
-                alt="GitHub Profile Picture"
-                width={40}
-                height={40}
-                className="mb-12 rounded-full bg-white md:mb-0 md:mr-2"
-              />
+              <Suspense
+                fallback={
+                  <Skeleton className="h-[42px] w-[42px] rounded-full" />
+                }
+              >
+                <GitHubAvatar />
+              </Suspense>
             </Link>
 
             <p className="text-center text-sm leading-loose md:text-left">
